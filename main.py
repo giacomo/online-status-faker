c226c8c4a08bc20349690b0bf8ca3b8a2bc716ce
Giacomo Barbalinardo <info@ready24it.eu>
2022-03-02 08:36:00 +0100

    choose mode between mouse or keyboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pynput import keyboard
 import pyautogui
 import time
+import sys
 
 class OnlineStatusFaker:
     # define some pixels
@@ -23,17 +24,34 @@ class OnlineStatusFaker:
             # move the mouse cursor
             pyautogui.moveRel(0, self.pixel, duration=.2)
 
+    def start_keyboard(self):
+        print('Keyboard started...')
+        while self.condition:
+            # press and release the space key
+            controller = keyboard.Controller()
+            controller.press(keyboard.Key.space)
+            controller.release(keyboard.Key.space)
+            time.sleep(.5)
+            controller.press(keyboard.Key.backspace)
+            controller.release(keyboard.Key.backspace)
+            time.sleep(.5)
+        
+
     def delay_start(self):
         for seq in range(self.delay):
             print(self.delay - seq)
             time.sleep(1)
 
     def __init__(self):
+        mode = input('Choose mode: (m)ouse or (k)eyboard: ')
         print('Starting Online-Faker in {0} seconds...'.format(self.delay))
         print('You can quit the application pressing the ESC key.')
         listener = keyboard.Listener(on_release=self.on_release)
         listener.start()
         self.delay_start()
-        self.start_mouse()
+        if mode == 'm':
+            self.start_mouse()
+        elif mode == 'k':
+            self.start_keyboard()
 
 OnlineStatusFaker()
